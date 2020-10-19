a9324201fb61975af2bba9c01618d964b6180425
jason <j.s.pursey@gmail.com>
2020-10-19 17:26:18 +1100

    Add integration tests for companion

---
+++ companion/tests/integration/index.js
@@ -0,0 +1,63 @@
+import test from 'ava'
+import * as sinon from 'sinon'
+import proxyquire from 'proxyquire'
+
+const messagingStub = {
+    peerSocket: {
+        send: sinon.stub()
+    },
+    '@noCallThru': true
+}
+
+const ServiceStub = class {
+    initialize = sinon.stub().returns({ units: 'mmol' })
+
+    latestReading = sinon.stub().returns({
+        time: 1598763209167,
+        value: 201,
+        alarm: null
+    })
+}
+
+const companion = proxyquire('../../index', {
+    'messaging': messagingStub,
+    './services/nightscout': { NightscoutService: ServiceStub },
+    './services/xdrip': { XdripService: ServiceStub },
+    './services/tomato': { TomatoService: ServiceStub }
+})
+
+test('companion exports intialize() function', t => {
+    t.is(typeof companion.initialize, 'function')
+})
+
+test('companion exports GuhrliError', t => {
+    t.is(typeof companion.GuhrliError, 'function')
+    const instance = new companion.GuhrliError('msg')
+    t.is(instance instanceof Error, true)
+})
+
+test('companion.initialize throws GuhrliError if called without source', t => {
+    t.throws(() => companion.initialize(), companion.GuhrliError)
+})
+
+test('companion.initialize() with source: "TOMATO" returns GuhrliCompanion instance', t => {
+    const guhrli = companion.initialize({ source: 'TOMATO' })
+    t.is(typeof guhrli, 'object')
+    t.is(typeof guhrli.fetchReading, 'function')
+})
+
+test('companion.initialize() with source: "XDRIP" returns GuhrliCompanion instance', t => {
+    const guhrli = companion.initialize({ source: 'XDRIP' })
+    t.is(typeof guhrli, 'object')
+    t.is(typeof guhrli.fetchReading, 'function')
+})
+
+test('companion.initialize() with source: "NIGHTSCOUT" and URL returns GuhrliCompanion instance', t => {
+    const guhrli = companion.initialize({ source: 'NIGHTSCOUT', nightscoutURL: 'https://test.com/' })
+    t.is(typeof guhrli, 'object')
+    t.is(typeof guhrli.fetchReading, 'function')
+})
+
+test('companion.initialize() with source: "NIGHTSCOUT" and no URL throws GuhrliError', t => {
+    t.throws(() => companion.initialize({source: 'NIGHTSCOUT'}))
+})
